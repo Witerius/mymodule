462b5788de39b0ea7f4e5fd1213196a89726392c
mikhail <wizardmb@gmail.com>
2021-04-18 18:27:58 +0600

    4

--- main/main.go
+++ main/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	//_ "github.com/gorilla/websocket"
 	"fmt"
 	_ "github.com/valyala/fasthttp"
 )
 
 func main() {
-	fmt.Print("Hello World!")
+	fmt.Print("Hello World!!!")
 }
