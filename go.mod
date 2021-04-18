d7c1d1f913a6b3a8f4883dbb75035b5c58914ca8
mikhail <wizardmb@gmail.com>
2021-04-18 18:15:19 +0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Witerius/mymodule
+
+go 1.16
+
+require github.com/valyala/fasthttp v1.23.0
---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	//_ "github.com/gorilla/websocket"
+	"fmt"
+	_ "github.com/valyala/fasthttp"
+)
+
+func main() {
+	fmt.Print("Hello")
+}
